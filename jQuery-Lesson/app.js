4c8fea773853bf47c885782799408d6f3ba093a7
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-09-04 02:04:07 +0000

    まとめ

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,48 @@
+/* $(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+}); */
+
+/* $(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
+}); */
+
+/* $(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+}); */
+
+/* $(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+}); */
+
+/* $(function(){
+  $('.box1').on('click', function(){
+    $(this).slideUp();
+  });
+}); */
+
+$(function(){
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
+  });
+});
